70c14b442ee15d585b439e934f14ccf1a0be7cca
XerK <gm.xerk@gmail.com>
2019-02-11 06:33:45 +0200

    Avatar emp

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -40,7 +40,22 @@ class ResultController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        DB::inser('INSERT INTO motaba.results 
+                        (user_id, sup_subject_id, exam_id, exam1)
+                        SELECT 
+                            users.id, subjects_d.id, subjects_d.exam_id, 0
+                            
+                        FROM 
+                            ((SELECT * FROM users WHERE job = 1 AND users.class_id = 36) as users
+                        CROSS JOIN
+                            (SELECT sup_subjects.id, sup_subjects.subject_id, sup_subjects.exam_id, subjects.name, subjects.class_edu_id FROM motaba.sup_subjects
+                                INNER JOIN subjects ON subjects.id = sup_subjects.subject_id
+                                    WHERE subjects.class_edu_id = 36) as subjects_d) 
+                            ON 
+                DUPLICATE 
+                KEY UPDATE
+                                user_id=users.id, sup_subject_id=subjects_d.id, exam_id=subjects_d.exam_id');
+        return 'Successfully Added';
     }
 
     /**
